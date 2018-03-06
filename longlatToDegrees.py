37d0d88bd69f0f3bfd09f4f5f72d82052dbf7980
sarthak bhagat <sarthakbhagat@gmail.com>
2018-03-07 00:26:20 +0530

    made convert2deg

---
+++ longlatToDegrees.py
@@ -0,0 +1,11 @@
+def convert(deg,min,sec):
+	mindeg = 0.0166667*min
+	secdeg = 0.00027777833333*sec
+	deg = mindeg + secdeg + deg
+	print (deg)
+
+deg = input ("Enter degrees : ")
+mindeg = input("Enter Minutes of Arc : ")
+secdeg = input("Enter Seconds of Arc : ")
+
+convert(deg,mindeg,secdeg)
