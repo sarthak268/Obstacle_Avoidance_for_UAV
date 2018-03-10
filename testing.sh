4adb7f2f52b728d1e96553c5d66f4c3b912f1b04
sarthak bhagat <sarthakbhagat@gmail.com>
2018-03-10 16:13:11 +0530

    script for testing complete

--- testing.sh
+++ testing.sh
@@ -1,4 +1,12 @@
-xterm -e roscore
-xterm -e cd /home/sarthak/Desktop/ardupilot/ArduPlane | /home/sarthak/Desktop/ardupilot/Tools/autotest/sim_vehicle.py --console --map --aircraft test
-xterm -e roslaunch mavros apm2.launch fcu_url:=udp://localhost:14550@
-xterm -e rosrun map currentXY
+#!/bin/bash
+
+# This script can be used when we use the workspace for testing by connecting it to MAVROS 
+
+roscore & roslaunch mavros apm2.launch fcu_url:=udp://localhost:14550@ & rosrun map currentXY & rosrun map markPoints & rviz & rosrun tf static_transform_publisher 0 0 0 0 0 0 1 map my_frame 10 & rosrun map waypoints 
+
+
+# will have to run command separately:
+
+# cd /home/sarthak/Desktop/ardupilot/ArduPlane
+# /home/sarthak/Desktop/ardupilot/Tools/autotest/sim_vehicle.py --console --map --aircraft test
+ 
